21be4040b5cbcd1e656eac59d288add1ff7d9124
Oleh_Vls <olehvlasyik@gmail.com>
2017-03-08 00:13:30 +0200

    What we do - page

--- js/main.js
+++ js/main.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
     },1500);
 
     myParaxify = paraxify('.about-us');
+
+    var wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       100,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
+
+    $('a[class="scroll-to-block"]').on('click',function(event){
+        event.preventDefault();
+        var target = $(this).attr('data-scroll');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+        return false;
+    });
+
+
 });
